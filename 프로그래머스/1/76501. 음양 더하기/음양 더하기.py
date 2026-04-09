41a18a8413e99fa5806041e417dcc603658985de
Sangdal Lee <asksa1256@gmail.com>
2026-04-09 23:32:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,7 +1,7 @@
 def solution(absolutes, signs):
     total = 0
     for i in range(len(signs)):
-        if (signs[i]):
+        if signs[i]:
             total += absolutes[i]
         else:
             total -= absolutes[i]
