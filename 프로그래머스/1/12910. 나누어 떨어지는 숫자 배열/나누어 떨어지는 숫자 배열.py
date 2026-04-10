a73609a7087221dae0ba985e19d01d67a01c3948
Sangdal Lee <asksa1256@gmail.com>
2026-04-10 21:33:29 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.26 ms, Memory: 12.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,11 @@
+def solution(arr, divisor):
+    # result = []
+    # for i in range(len(arr)):
+    #     if arr[i] % divisor == 0:
+    #         result.append(arr[i])
+    # result.sort()
+    # return result if len(result) > 0 else [-1]
+    
+    result = sorted([x for x in arr if x % divisor == 0])
+    
+    return result if len(result) else [-1]
